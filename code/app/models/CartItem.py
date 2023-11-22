6ca9a89f96998d86d9affbcf5053a2d37d923046
Yuv <billiondollarone07@gmail.com>
2023-11-22 16:06:02 +0530

    Renamed CartItems model

--- code/app/models/CartItem.py
+++ code/app/models/CartItem.py
@@ -1,6 +1,6 @@
 from ..utils import db
 
-class CartItems(db.Model):
+class CartItem(db.Model):
     __tablename__='UserCartItems'
     id = db.Column(db.Integer, autoincrement=True, unique=True, primary_key=True, nullable=False)
     product = db.Column(db.Integer, nullable=True)
