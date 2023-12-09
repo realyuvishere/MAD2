deeef34fdc5cadd5972faea474ebfe14e3c837b4
Yuv <billiondollarone07@gmail.com>
2023-12-09 13:22:08 +0000

    requests

--- code/app/views/auth.py
+++ code/app/views/auth.py
@@ -1,14 +1,25 @@
-from flask import render_template, jsonify, current_app as app, request, redirect, url_for
-from datetime import datetime
+from flask import current_app as app, request
+from ..utils import request_error, request_ok
 
 @app.route('/auth/login', methods=['POST'])
 def login():
-    return jsonify({'message': 'okay'}), 400
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+
+    if not email:
+        return request_error(message="Email is required")
+    
+    if not password:
+        return request_error(message="Password is required")
+
+    return request_ok(message="User authorized")
 
 @app.route('/auth/signup', methods=['POST'])
 def signup():
-    return render_template('user_cart_item_delete.html')
+    return request_ok(message="User signed up.")
 
 @app.route('/auth/user_types', methods=['GET'])
 def get_user_types():
-    return render_template('user_cart_item_delete.html')
+    return request_ok(message="Fetched user types.")
+
