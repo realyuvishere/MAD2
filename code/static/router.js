3a06f7ac1d5913e881c977dfd973a8c99c30acea
Yuv <billiondollarone07@gmail.com>
2023-12-15 16:18:32 +0530

    search component cleanup

--- code/static/router.js
+++ code/static/router.js
@@ -8,14 +8,12 @@ import AdminCategories from './pages/AdminCategories.js'
 import ManagerProducts from './pages/ManagerProducts.js'
 import Invoices from './pages/Invoices.js'
 import AdminManagers from './pages/AdminManagers.js'
-import Search from './pages/Search.js'
 
 const routes = [
     { path: '/', component: Home, name: 'Home' },
     { path: '/login', component: Login, name: 'Login' },
     { path: '/signup', component: Signup, name: 'Sign up' },
     { path: '/invoices', component: Invoices, name: 'Invoices' },
-    { path: '/search', component: Search, name: 'Search' },
     { path: '/admin/users', component: AdminUsers, name: 'Admin user dashboard' },
     { path: '/admin/managers', component: AdminManagers, name: 'Admin manager dashboard' },
     { path: '/admin/category', component: AdminCategories, name: 'Admin category dashboard' },
